d0ae444be9f097449ca4f4f1b2fc21b567c55bac
formellaalex <formella.alex@gmail.com>
2015-10-02 19:24:44 +0200

    Fix bugs with validation and unnecessary objects

--- routes/controller.js
+++ routes/controller.js
@@ -1,3 +1,4 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
@@ -9,9 +10,9 @@ var pong = {"pong": true};
 
 
 function updateNote(note, next){
-	"use strict"
+	
 	var update = db.prepare("UPDATE NOTES SET TITLE = ?, MESSAGE = ?, MODIFY_DATE = ? WHERE ID = ?");
-	update.run(note.title, note.message, note.createDate, note.id, function(err) {
+	update.run(note.title, note.message, note.modifyDate, note.id, function(err) {
 		if(err) return next("Invalid ID. SQLITE3 ERROR: " + err);
 		return next(null,note);
 			
@@ -20,7 +21,6 @@ function updateNote(note, next){
 
 
 function deleteNote(id, next){
-	"use strict"
 	var delete_q = db.prepare("DELETE FROM NOTES WHERE ID = ?");
  	
 	delete_q.run(id, function(err) {
@@ -31,7 +31,6 @@ function deleteNote(id, next){
 }
 
 function sendResponse(object,req,res){
-	"use strict"
 	if("format" in req.query){
 		if(req.query.format === "jsonp"){
 			res.jsonp(object);
@@ -46,7 +45,6 @@ function sendResponse(object,req,res){
 }
 //IMPORTANT! SQLITE3 for node.js don't handle errors (for example "OUT OF RANGE") so this function is not working properly
 function checkError(err,req,res){
-	"use strict"
 	errorInfo.message = err.code;
 	errorInfo.code = err.errno;
 	if(err.errno === 25 || err.errno === 16 || err.errno === 1){
@@ -59,42 +57,56 @@ function checkError(err,req,res){
 
 
 db.serialize(function() {
-	"use strict"
   	db.run("CREATE TABLE IF NOT EXISTS NOTES (ID INTEGER PRIMARY KEY AUTOINCREMENT, TITLE TEXT, MESSAGE TEXT, CREATE_DATE DATETIME DEFAULT CURRENT_TIMESTAMP, MODIFY_DATE DATETIME)");
 });
 
-router.get('/v1/ping', function(req,res){
-	"use strict"
+router.get('/:version/ping', function(req,res){
     sendResponse(pong,req, res);
 });
 
-router.get('/v1/notes', function(req,res){
-	"use strict"
+router.get('/:version/notes', function(req,res){
+
 	db.all("SELECT * FROM NOTES", function(err, selectRes){
 		if(err) sendResponse(err.toString(),req, res);
-		else sendResponse(selectRes,req, res);
+		else{
+			var responseArray = {};
+			responseArray.count = selectRes.length;
+			responseArray.results = selectRes;
+			sendResponse(responseArray,req, res);
+		} 
 	});
 });
 
 
-router.get('/v1/notes/:id', function(req,res){
-	"use strict"
+router.get('/:version/notes/:id', function(req,res){
 	db.all("SELECT * FROM NOTES WHERE ID=" + req.params.id, function(err, selectRes){
 		if(err) sendResponse(err.toString(),req, res);
 		else sendResponse(selectRes,req, res);
 	});
 });
 
-router.post('/v1/notes/', function(req,res){
-	"use strict"
-	var note = [req.query.title, req.query.message, new Date().toJSON().slice(0,10), new Date().toJSON().slice(0,10)];
-	db.run("INSERT INTO NOTES(TITLE, MESSAGE, CREATE_DATE, MODIFY_DATE) VALUES(?,?,?,?)",note);
-	sendResponse(note, req,res);
+router.post('/:version/notes/', function(req,res){
+	if(!req.query.title || !req.query.message){
+		sendResponse("404 - Not Found \n Invalid parameters in query string.",req, res);
+	}
+	else{
+		var note = [req.query.title, req.query.message, new Date().toJSON().slice(0,10), new Date().toJSON().slice(0,10)];
+		db.run("INSERT INTO NOTES(TITLE, MESSAGE, CREATE_DATE, MODIFY_DATE) VALUES(?,?,?,?)",note);
+		sendResponse(note, req,res);
+	}
+	
 });
 
-router.put('/v1/notes/:id', function(req,res){
-	"use strict"
-	var note = {id:req.params.id,title: req.query.title, message: req.query.message, createDate: new Date().toJSON().slice(0,10)};
+router.put('/:version/notes/:id', function(req,res){
+	var note = {};
+	if(req.query.title){
+		note.title = req.query.title;
+	}
+	if(req.query.message){
+		note.message = req.query.message;
+	}
+	note.modifyDate = new Date().toJSON().slice(0,10);
+	note.id = req.params.id;
 	updateNote(note, function(err, updateResult){
 		if(err) checkError(err,req,res); // sqlite3 can't handle these errors.
 		else sendResponse(updateResult,req,res);
@@ -103,8 +115,7 @@ router.put('/v1/notes/:id', function(req,res){
 });
 
 
-router.delete('/v1/notes/:id', function(req,res){
-	"use strict"
+router.delete('/:version/notes/:id', function(req,res){
 	var id = req.params.id;
 	deleteNote(id, function(err, deleteResult){
 		if(err) checkError(err,req,res); // sqlite3 can't handle these errors.
